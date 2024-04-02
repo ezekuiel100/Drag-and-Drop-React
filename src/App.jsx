eb2e145ad5a4163bad51287b89a135df18c8a560
ezekuiel100 <ezekuiel100@gmail.com>
2024-04-02 17:59:56 -0300

    Refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import { useState } from "react";
 
 function App() {
   const [cards, setCards] = useState([
-    { id: 1, text: "conteudo 1" },
-    { id: 2, text: "conteudo 2" },
-    { id: 3, text: "conteudo 3" },
-    { id: 4, text: "conteudo 4" },
-  ]);
+    [
+      { id: 1, text: "conteudo 1" },
+      { id: 2, text: "conteudo 2" },
+      { id: 3, text: "conteudo 3" },
+      { id: 4, text: "conteudo 4" },
+    ],
 
-  const [card2, setCard2] = useState([]);
+    [],
+  ]);
 
   function handleDragEnd(result) {
     if (!result.destination) return; // Se o cartão foi descartado fora de uma área droppable, saia da função
@@ -17,103 +19,72 @@ function App() {
     const sourceIndex = result.source.index;
     const destinationIndex = result.destination.index;
 
-    const destination = result.destination.droppableId;
     const source = result.source.droppableId;
+    const destination = result.destination.droppableId;
 
     let updatedCards = [...cards];
-    let updatedCard2 = [...card2];
 
-    if (source === "cards" && destination === "cards") {
-      const [dragged] = updatedCards.splice(sourceIndex, 1);
-      updatedCards.splice(destinationIndex, 0, dragged);
+    if (source === destination && source === "card1") {
+      const [removed] = updatedCards[0].splice(sourceIndex, 1);
+      updatedCards[0].splice(destinationIndex, 0, removed);
       setCards(updatedCards);
     }
-    if (source === "card2" && destination === "card2") {
-      const [dragged] = updatedCard2.splice(sourceIndex, 1);
-      updatedCard2.splice(destinationIndex, 0, dragged);
-      setCard2(updatedCard2);
-    }
 
-    if (source === "cards" && destination === "card2") {
-      let [removed] = updatedCards.splice(sourceIndex, 1);
-      setCards(updatedCards);
-      updatedCard2 = [...card2, removed];
-      setCard2(updatedCard2);
-    }
-
-    if (source === "card2" && destination === "cards") {
-      let [removed] = updatedCard2.splice(sourceIndex, 1);
-      setCard2(updatedCard2);
-      updatedCards = [...cards, removed];
+    if (source === destination && source === "card2") {
+      const [removed] = updatedCards[1].splice(sourceIndex, 1);
+      updatedCards[1].splice(destinationIndex, 0, removed);
       setCards(updatedCards);
     }
-  }
 
-  function removeCard(card, sourceIndex) {
-    return card.splice(sourceIndex, 1); // Extrai o cartão arrastado
+    if (source !== destination && source === "card1") {
+      const [removed] = updatedCards[0].splice(sourceIndex, 1);
+      updatedCards[1].splice(destinationIndex, 0, removed);
+      setCards(updatedCards);
+    }
+
+    if (source !== destination && source === "card2") {
+      const [removed] = updatedCards[1].splice(sourceIndex, 1);
+      updatedCards[0].splice(destinationIndex, 0, removed);
+      setCards(updatedCards);
+    }
   }
 
   return (
     <DragDropContext onDragEnd={handleDragEnd}>
       <div className="flex gap-4 h-screen justify-center items-center">
-        <Droppable droppableId="cards">
-          {(provided) => (
-            <div
-              className="bg-blue-200 w-96 flex flex-col gap-4  p-8"
-              ref={provided.innerRef}
-              {...provided.droppableProps}
-            >
-              {cards.map((card, index) => (
-                <Draggable
-                  key={card.id}
-                  draggableId={card.id.toString()}
-                  index={index}
+        {cards.map((card, i) => {
+          return (
+            <Droppable droppableId={"card" + (i + 1)} key={i}>
+              {(provided) => (
+                <div
+                  className="bg-blue-200 w-96 flex flex-col gap-4  p-8"
+                  ref={provided.innerRef}
+                  {...provided.droppableProps}
                 >
-                  {(provided) => (
-                    <p
-                      className="p-2 bg-red-200"
-                      ref={provided.innerRef}
-                      {...provided.draggableProps}
-                      {...provided.dragHandleProps}
+                  {card.map((card, index) => (
+                    <Draggable
+                      key={card.id}
+                      draggableId={card.id.toString()}
+                      index={index}
                     >
-                      {card.text}
-                    </p>
-                  )}
-                </Draggable>
-              ))}
-              {provided.placeholder}
-            </div>
-          )}
-        </Droppable>
-        <Droppable droppableId="card2">
-          {(provided) => (
-            <div
-              className="bg-blue-200 w-96 flex flex-col gap-4  p-8"
-              ref={provided.innerRef}
-              {...provided.droppableProps}
-            >
-              {card2.map((card, index) => (
-                <Draggable
-                  key={card?.id}
-                  draggableId={card.id.toString()}
-                  index={index}
-                >
-                  {(provided) => (
-                    <p
-                      className="p-2 bg-red-200"
-                      ref={provided.innerRef}
-                      {...provided.draggableProps}
-                      {...provided.dragHandleProps}
-                    >
-                      {card.text}
-                    </p>
-                  )}
-                </Draggable>
-              ))}
-              {provided.placeholder}
-            </div>
-          )}
-        </Droppable>
+                      {(provided) => (
+                        <p
+                          className="p-2 bg-red-200"
+                          ref={provided.innerRef}
+                          {...provided.draggableProps}
+                          {...provided.dragHandleProps}
+                        >
+                          {card.text}
+                        </p>
+                      )}
+                    </Draggable>
+                  ))}
+                  {provided.placeholder}
+                </div>
+              )}
+            </Droppable>
+          );
+        })}
       </div>
     </DragDropContext>
   );
